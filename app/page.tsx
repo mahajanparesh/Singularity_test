bd21bd5570a1a236830de6db186bb0b09f2a1ad0
omkarpatne6 <omkarpatne6@gmail.com>
2023-11-18 14:15:46 +0530

    added new ThirdWeb module

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import { ThirdwebProvider } from "../components/ThirdwebProvider";
+import { Sepolia } from "@thirdweb-dev/chains";
+
+const activeChain = Sepolia;
 
 // Always remember this will be our root component. Consider this index file. Do not change file name
 const HomePage = () => {
   return (
-    <>
+    <ThirdwebProvider
+      activeChain={activeChain}
+      clientId="YOUR_CLIENT_ID" // You can get a client id from dashboard settings
+    >
       <Header />
       <Hero />
-    </>
+    </ThirdwebProvider>
   );
 };
 
